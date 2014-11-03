624c9ecdc492f8d1c1548dc721d91f44a48a86fb
Matthew Dunsdon <mdunsdon@scottlogic.com>
2014-11-03 16:09:34 +0000

    Add assert JS/css functionality for use by codepen
    Allows codepens to contain asserts with a pass or fail badge.

---
+++ js/assert.js
@@ -0,0 +1,10 @@
+var assert = function (actual, expectation, message) {
+    'use strict';
+    var outcome = arguments.length === 2 ? actual : actual === expectation,
+        description = arguments.length === 2 ? expectation : message,
+        para = document.createElement('p'),
+        text = document.createTextNode(description);
+    para.className = outcome ? 'pass' : 'fail';
+    para.appendChild(text);
+    document.body.appendChild(para);
+};
