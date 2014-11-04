db2e1b88cdd98e68e6869b52a27c1993148f8648
Matthew Dunsdon <mdunsdon@scottlogic.com>
2014-11-04 22:08:25 +0000

    Introduce iffe pattern for codepen code

--- js/assert.js
+++ js/assert.js
@@ -1,20 +1,26 @@
-var assert = function (actual, expectation, message) {
+(function () {
     'use strict';
-    var outcome = arguments.length === 2 ? actual : actual === expectation,
-        description = arguments.length === 2 ? expectation : message,
-        para = document.createElement('p'),
-        text = document.createTextNode(description);
-    para.className = outcome ? 'pass' : 'fail';
-    para.appendChild(text);
-    document.body.appendChild(para);
-};
 
-var assertIsNaN = function (nanValue, description) {
-    'use strict';
-    var outcome = isNaN(nanValue),
-        para = document.createElement('p'),
-        text = document.createTextNode(description);
-    para.className = outcome ? 'pass' : 'fail';
-    para.appendChild(text);
-    document.body.appendChild(para);
-};
+    function assert(actual, expectation, message) {
+        var outcome = arguments.length === 2 ? actual : actual === expectation,
+            description = arguments.length === 2 ? expectation : message,
+            para = document.createElement('p'),
+            text = document.createTextNode(description);
+        para.className = outcome ? 'pass' : 'fail';
+        para.appendChild(text);
+        document.body.appendChild(para);
+    }
+
+    function assertIsNaN(nanValue, description) {
+        var outcome = isNaN(nanValue),
+            para = document.createElement('p'),
+            text = document.createTextNode(description);
+        para.className = outcome ? 'pass' : 'fail';
+        para.appendChild(text);
+        document.body.appendChild(para);
+    }
+
+    window.assert = assert;
+
+    window.assertIsNaN = assertIsNaN;
+}());
